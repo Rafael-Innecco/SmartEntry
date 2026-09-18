b1a2faae05b6de06a06c18eef2f4947b96034bb1
Rafael Innecco <rafael.a.innecco@usp.br>
2026-09-18 15:40:58 -0300

    Kickstart lock with hello_world

---
+++ fechadura/build/CMakeFiles/4.0.3/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-7.2.4-200.fc44.x86_64")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "7.2.4-200.fc44.x86_64")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/rinnecco/poli/2026.3/lab_embarcados/SmartEntry/fechadura/build/toolchain/toolchain-esp32c3.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
